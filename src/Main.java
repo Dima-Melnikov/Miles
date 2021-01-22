1b8ce875b2614ac5c82f8f94e780f57635ec9ea7
Dima Melnikov <diman217@bk.ru>
2021-01-22 17:05:56 +0300

    firts

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 4500;
+        int bonusmiles = 1;
+        int rubles = 20;
+        int totalbonus = price * bonusmiles / rubles;
+        System.out.println(totalbonus);
+
+    }
+}
